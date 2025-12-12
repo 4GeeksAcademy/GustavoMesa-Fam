c984c8d4cdcfd0ce68e981ecc1b92fe44af0eab6
Gustavo Mesa <gustavoamesa17@gmail.com>
2025-12-12 17:26:29 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -30,12 +30,56 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def members():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {"hello": "world",
                      "family": members}
     return jsonify(response_body), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def menber(member_id):
+    response_body = {}
+    if request.method == 'GET':
+        response_body['message'] = 'Integrante de la familia'
+        response_body['results'] = jackson_family.get_member(member_id)
+        print(response_body)
+        if response_body['results']:
+            return response_body, 200
+        response_body['message'] = 'Error en la petición, solicitud o ID no encotrado'
+        return response_body, 400
+    
+@app.route('/member', methods=['POST'])
+def member():
+    response_body = {}
+    if request.method == 'POST':
+        data = request.json 
+        first_name = data.get('first_name', 'Desconocido')
+        age = data.get('age', 0)
+        numbers = data.get('lucky_number',[0])
+        new_data = { 'first_name': first_name,
+                     'age': age,
+                     'lucky_number': numbers}
+        response_body['results'] = new_data
+        print(response_body)
+        if response_body['results']:
+            return response_body, 200
+        response_body['message'] = 'Error en el envio de datos verificar por favor'
+        return response_body, 400
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def menber_delete(member_id):
+    response_body = {}
+    if request.method == 'DELETE':
+        response_body['message'] = 'Se ha eliminado el integrante de la familia'
+        response_body['results'] = jackson_family.delete_member(member_id)
+        print(response_body)
+        if response_body['results']:
+            return response_body, 200
+        response_body['message'] = 'Error en la petición, solicitud o ID no encotrado'
+        return response_body, 400
+
+
 
 
 
